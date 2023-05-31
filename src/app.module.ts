c083608f22041f4691ca970e7e3d513085d338e3
Gusb <mihivanmih@gmail.com>
2023-05-31 23:09:24 +0300

    start - bd postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { FilesModule } from './files/files.module';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {UserEntity} from "./users/entities/user.entity";
+import {FileEntity} from "./files/entities/file.entity";
 
 @Module({
-  imports: [UsersModule],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'lucky.db.elephantsql.com',
+    port: 5432,
+    username: 'gyeswyrv',
+    password: 'rqWv0qYBzYDUZ-ZX_rlqfTva_2zH5ve4',
+    database: 'gyeswyrv',
+    entities: [UserEntity, FileEntity],
+    synchronize: true,
+  }),
+    UsersModule, FilesModule],
   controllers: [AppController],
   providers: [AppService],
 })
