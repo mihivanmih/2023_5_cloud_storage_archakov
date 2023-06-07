57cbe85f8785901612b7363c58686459e6ffbfdc
Gusb <mihivanmih@gmail.com>
2023-06-07 23:26:46 +0300

    file bd

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -2,20 +2,17 @@ import {
   Controller,
   Get,
   Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
   UseInterceptors,
   UploadedFile,
-  ParseFilePipe, MaxFileSizeValidator, UseGuards
+  ParseFilePipe, MaxFileSizeValidator, UseGuards, Query
 } from '@nestjs/common';
 import { FilesService } from './files.service';
-import { CreateFileDto } from './dto/create-file.dto';
 import {ApiBearerAuth, ApiBody, ApiConsumes, ApiTags} from "@nestjs/swagger";
 import {FileInterceptor} from "@nestjs/platform-express";
 import {fileStorage} from "./storage";
 import {JwtAuthGuard} from "../auth/guards/jwt.guard";
+import {UserId} from "../decorators/user-id.decorator";
+import {FileType} from "./entities/file.entity";
 
 @Controller('files')
 @ApiTags('files')
@@ -25,8 +22,8 @@ export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
   @Get()
-  findAll() {
-    return this.filesService.findAll()
+  findAll(@UserId() userId: number, @Query('type') fileType: FileType) {
+    return this.filesService.findAll(userId, fileType)
   }
 
   @Post()
@@ -53,7 +50,10 @@ export class FilesController {
       new ParseFilePipe({
         validators: [new MaxFileSizeValidator({ maxSize: 1024 * 1024 * 5})]
       })
-  ) file: Express.Multer.File) {
-    return file
+  )
+         file: Express.Multer.File,
+         @UserId() userId: number
+  ) {
+    return this.filesService.create(file, userId)
   }
 }
